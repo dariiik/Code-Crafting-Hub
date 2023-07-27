43995d958d6f6c571fdef77a30cf048a4b02e60b
dariiik <122713640+dariiik@users.noreply.github.com>
2023-07-27 14:06:10 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,20 @@ var http = require('http');
 
 var host = '127.0.0.1'; 
 var port = '3000'; 
-
-var server = http.createServer(function(req, res){
-    res.statusCode = 200; 
-    res.setHeader('Content-Type', 'text/plain'); 
-    res.end('Hello World');
-}); 
-server.listen(port, host, function(){
-    console.log('Server running on port 2000');
+//check if there is an html file// 
+fs.readFile('./index.html', function(err, html){
+    if(err){
+        console.log(err);
+        return;
+    }
+    
+    var server = http.createServer(function(req, res){
+        res.statusCode = 200; 
+        res.setHeader('Content-Type', 'text/plain'); 
+        res.write(html); 
+        res.end();
+    }); 
+    server.listen(port, host, function(){
+        console.log('Server running on port 3000');
+    });
 });
